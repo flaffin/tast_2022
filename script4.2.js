9b707a193d4909d1be1435d947582a552b348542
Guskova <flaffin@inbox.ru>
2022-03-22 17:36:36 +0300

    homework4hard

---
+++ script4.2.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//  Задача 2
+
+function returnString(arg){
+
+    if (typeof arg === 'string'){
+       arg = arg.trim();
+        console.log(arg);
+        
+       let lengthString = arg.length;
+        console.log(lengthString);
+
+            if (lengthString > 30) {
+                arg = arg.substring(0, 30);
+                arg= arg + '...';
+                console.log(arg);
+            }
+} else {
+    console.log('Не строка');
+}
+}
+returnString('    привет, как дела. Сегодня хорошая погода   ')
